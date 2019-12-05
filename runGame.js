4b6638fb079b89bbfa6dcbb5d7a315db31d07c1f
Abdullah Binmahfouz <abdullahbinmahfouz@Abdullah-Binmahfouz.local>
2019-12-05 09:04:36 +0300

    missing the winning part

--- runGame.js
+++ runGame.js
@@ -1,51 +1,136 @@
+
+
+var changePlayer = 0;
+
+// 
+
 $(document).ready(function () {
+    
+        $("[id*='cell-']").on('click', function (event) 
+                    {
+                   
+                if (changePlayer ===0)
+                {
+                    var x = this.cellIndex;                   
+                    // var y= this.parentElement.rowIndex;
+                    var played = false;
+
+                    for (i = this.parentElement.parentElement.rows.length -1; i >= 0; i--) {
+                        var currentCell = this.parentElement.parentElement.rows[i].cells[x]
+                        var check = currentCell.classList.contains('red');
+                        if(!(check || currentCell.classList.contains('green')))
+                        {
+                            this.parentElement.parentElement.rows[i].cells[x].classList.add("red");
+                            played = true;
+                            break;
+                        }
+                      }
+                    
+
+                $(this).off("mouseleave"); 
+                //$(this).addClass('red');
+            
+                
+                // winFunction()
+                if(played)
+                {
+                changePlayer ++;
+                }
+                }
+                    
+                
+                else if(changePlayer ===1)
+                { 
+                    var x = this.cellIndex;                   
+                    var y= this.parentElement.rowIndex;
+                    var played = false;
+                    
+
+                    for (i = this.parentElement.parentElement.rows.length -1; i >= 0; i--) {
+                        var currentCell = this.parentElement.parentElement.rows[i].cells[x]
+                        var check = currentCell.classList.contains('red');
+                        if(!(check || currentCell.classList.contains('green')))
+                        {
+                            played = true;
+                            this.parentElement.parentElement.rows[i].cells[x].classList.add("green");
+                            break;
+                        }
+                      }
+                $(this).off("mouseleave");
+               
+               // $(this).addClass('green');
+            
+               if(played)
+                {
+                changePlayer --;
+                }
+                } 
+            
+                    
+              
+            });
 
 
+         
 
-
-    $("[id*='cell-']").click(function () {
-        var finished = 0;
-
-        if (finished == 0) {
-            $(this).addClass("green");
-            $(this).off("mouseleave");
-            finished++ + ;
-
+                    
+        });
+        
+ 
+        function winFunction() {
+            var counter = 0;
+            $("[id*='cell-']").each(function (index) {
+                if ($(this).hasClass('green')) {
+                    counter++;
+                }
+                if (counter === 3) {
+                    var $winner = $("<h2/>").text("congratulations!");
+                    $(".red").append($winner);
+                   
+                }
+            });
         }
-        if (finished == 0) {
-            $(this).addClass("red");
-            $(this).off("mouseleave");
-            finished-- -
 
-
-            // ;
+//// create array for cell 
+        var cellIndexId=[];
+        for(let row=0; row<=15; row++)
+        {
+            cellIndexId[row] =["cell-"+[row]]
+            
         }
+var home =[]
+for(let x=0; x<=cellIndexId.length; x++)
+{
+        home[x] =document.querySelector(`#` + cellIndexId[x])
+
+}
+
+//get the giagnol
+// let dg1= [13,10,7,4]
+// let dg2=[1,6,11,16]
+// $("[id*='cell-']").each(function (index){
+// if ($(home[dg1]).hasClass('green'))
+// {
+// console.log("whats is lobe")
+// }
+
+// });
 
 
-    });
+      
+
+       
 
 
-    console.dir(finished)
 
-});
+      
 
-
-            // $(this).addClass("green");
-
-            // $(this).addClass("red");
-            // if (
-            //     // $("#cell-1").hasClass("green") &&
-            //     // $("#cell-2").hasClass("green") &&
-            //     // $("#cell-3").hasClass("green") &&
-            //     // $("#cell-4").hasClass("green")
-            // ) {
-            //     $(".container").append('');
-            // }
-
-    //
-    // if (click == 0)
+        // console.dir(ids)
 
 
-    // $("[id*='cell-']").mouseleave(function () {
-    //     
-    // });
+
+    
+
+
+
+
